805f2364163f828d0ecc0dc9ede7e7ad5c08a6fa
Nicholas Tran <nicholastran2002@gmail.com>
2023-11-06 22:51:50 -0600

    added comments for LM

--- model1.R
+++ model1.R
@@ -1,3 +1,12 @@
+# Linear Regression
+
+# READ DATASET
 library(readr)
 Sleep_Efficiency <- read_csv("repos/DSML_Group3_Project/Sleep_Efficiency.csv")
 View(Sleep_Efficiency)
+
+# CLEAN DATASET OF NULL VALUES
+
+# FACTOR ANY CATEGORICALS, NUMERIC ANY NUMBERS R DOESNT READ PROPERLY
+
+# CREATE THE LINEAR MODEL
