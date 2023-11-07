4216422fee01002ad09c7821a4827cf75aefee13
Nicholas Tran <nicholastran2002@gmail.com>
2023-11-07 12:37:56 -0600

    removed null values

--- model1.R
+++ model1.R
@@ -2,7 +2,7 @@
 
 # READ DATASET
 library(readr)
-Sleep_Efficiency <- read_csv("repos/DSML_Group3_Project/Sleep_Efficiency.csv")
+Sleep_Efficiency <- read_csv("Repositories/DSML_Group3_Project/Sleep_Efficiency.csv")
 View(Sleep_Efficiency)
 
 # CLEAN DATASET OF NULL VALUES
