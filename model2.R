4f1f9b58a5c8bf2e5ea65d491f067ac1a9277b58
Nicholas Tran <nicholastran2002@gmail.com>
2023-11-09 19:57:38 -0600

    fixed time variables for models

--- model2.R
+++ model2.R
@@ -1,11 +1,13 @@
-# Model TBD
+# Model - Decision Tree
 
 # Sleep Efficiency ~ Bedtime + Caffeine + Alcohol + Smoking Status + Exercise
 
-# READ DATASET
+#LIBRARIES
 library(readr)
+library(lubridate)
+
+# READ DATASET
 dataset = read_csv("Repositories/DSML_Group3_Project/Sleep_Efficiency.csv")
-View(dataset)
 
 # CLEAN DATASET OF NULL VALUES
 sum(is.na(dataset))
@@ -15,17 +17,14 @@ sum(is.na(dataset))
 # REMOVE ANY UNWANTED VARIABLES
 columns_to_keep <- c(4,7,12,13,14,15)
 dataset <- dataset[,columns_to_keep]
-View(dataset)
 
-# FIXED BETIME VARIABLE TO ONLY BE TIME
-library(lubridate)
+# FIXED BEDTIME VARIABLE TO ONLY BE TIME
 dataset$Bedtime <- ymd_hms(dataset$Bedtime)
-dataset$Time <- format(dataset$Bedtime, format = "%H:%M:%S")
-View(dataset)
+dataset$Bedtime <- format(dataset$Bedtime, format = "%H:%M:%S")
 
+# REMOVE ANY UNWANTED VARIABLES
 sum(is.na(dataset))
 dataset = na.omit(dataset)
 sum(is.na(dataset))
 
-dataset <- dataset[,-1]
 View(dataset)
