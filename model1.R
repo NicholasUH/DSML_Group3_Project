5a447a8f0c435fa994dc817b8d7466da32f5920e
Nicholas Tran <nicholastran2002@gmail.com>
2023-11-09 18:48:23 -0600

    removed unwanted variables

--- model1.R
+++ model1.R
@@ -1,5 +1,7 @@
 # Linear Regression
 
+# Sleep Efficiency ~ Bedtime + Caffeine + Alcohol + Smoking Status + Exercise
+
 # READ DATASET
 library(readr)
 dataset = read_csv("Repositories/DSML_Group3_Project/Sleep_Efficiency.csv")
@@ -7,10 +9,15 @@ View(dataset)
 
 # CLEAN DATASET OF NULL VALUES
 sum(is.na(dataset))
-new_dataset = na.omit(dataset)
-sum(is.na(new_dataset))
+dataset = na.omit(dataset)
+sum(is.na(dataset))
 
+# REMOVE ANY UNWANTED VARIABLES
+columns_to_keep <- c(4,7,12,13,14,15)
+dataset <- dataset[,columns_to_keep]
+View(dataset)
 
 # FACTOR ANY CATEGORICALS, NUMERIC ANY NUMBERS R DOESNT READ PROPERLY
+
 
 # CREATE THE LINEAR MODEL
